780a0839077e503a65c46e13be2d569269d4083a
cnsudstudent1 <cnsudstudent1@gmail.com>
2026-06-25 18:01:30 -0400

    left of on step 25

--- Assets/Scripts/MovingAndRotatingObjects.cs
+++ Assets/Scripts/MovingAndRotatingObjects.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingAndRotatingObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    //Default moving speed
+    [Header("Default Movement Speed")]
+    public float moveSpeed = 10f;
+    //Default Rotating Speed
+    [Header("Default Rotation Speed")]
+    public float rotateSpeed = 50f;
 
     // Update is called once per frame
     void Update()
     {
-
+        //object moving on the y axis based on move speed
+        transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        //rotate on the x axis based on rotate speed
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
     }
 }
